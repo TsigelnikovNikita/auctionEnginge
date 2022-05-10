de8b9cff7eca5e5ba2d7063320d223a67157679d
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-05-10 21:30:30 +0300

    added createAuction funciton

--- contracts/AucEngine.sol
+++ contracts/AucEngine.sol
@@ -21,7 +21,38 @@ contract AucEngine {
 
     Auction[] public auctions;
 
+    event AuctionCreated(uint index, string itemName, uint startingPrice, uint duration);
+
     constructor() {
-        owner = msg.owner;
+        owner = msg.sender;
     }
+
+    function createAuction(
+        uint _strartingPrice,
+        uint _discountRate,
+        string calldata _item,
+        uint _duration
+    ) external
+    {
+        require(_strartingPrice >= _discountRate * _duration, "incorrect starting price");
+
+        _duration = _duration == 0 ? DURATION : _duration;
+
+        Auction memory newAuciton = Auction({
+            seller: payable(msg.sender),
+            startingPrice: _strartingPrice,
+            finalPrice:  _strartingPrice,
+            discountRate: _discountRate,
+            startAt: block.timestamp,
+            endsAt: block.timestamp + _duration,
+            item : _item,
+            stopped: false
+        });
+
+        auctions.push(newAuciton);
+
+        emit AuctionCreated(auctions.length - 1, _item, _strartingPrice, _duration);
+    }
+
+    
 }
